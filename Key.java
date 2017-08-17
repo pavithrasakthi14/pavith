ce7f8d4ee8bb08cea2327db5d189832ee912ffe5
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-17 15:13:21 +0530

    Create Key.java

---
+++ Key.java
@@ -0,0 +1,22 @@
+import java.io.*;
+	import java.util.*;
+	public class Main
+	{
+	public static void main(String args[])throws IOException
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Q:");
+	while(sc.hasNext())
+	{
+		String s=sc.nextLine();
+		if(!(s.equals('q'))||!(s.equals('Q')))
+		{
+			
+		}
+		else
+		{
+		System.exit(0);
+	}
+	}
+	}
+	}
